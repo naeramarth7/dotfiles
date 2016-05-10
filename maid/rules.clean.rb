381b3d819b39f04cf5b8cb4a5788f62fec04f4d7
Sven Flickinger <hc4e2k@gmail.com>
2016-05-11 00:03:42 +0200

    add maid rule to "Take out the Trash"

--- maid/rules.clean.rb
+++ maid/rules.clean.rb
@@ -1,16 +1,42 @@
 Maid.rules do
 
-  rule 'Delete disk images from ~/Downloads' do
+  #
+  # Trash
+  # Take out the trash first to have the newly trashed files in there
+  #
+
+  rule 'Take out the Trash' do
+    dir('~/.Trash/*').each do |p|
+      remove(p) if accessed_at(p) > 20.days.ago
+    end
+  end
+
+  #
+  # Downloads
+  #
+
+  rule 'Trash disk images from ~/Downloads' do
     trash(dir('~/Downloads/*.dmg'))
   end
 
-  rule 'Delete applications in zip files from ~/Downloads' do
+  rule 'Trash applications in zip files from ~/Downloads' do
     found = dir('~/Downloads/*.zip').select { |path|
       zipfile_contents(path).any? { |c| c.match(/\.app$/) }
     }
 
     trash(found)
   end
+
+  rule 'Trash duplicate downloads' do
+    dupes_in('~/Downloads/*').each do |dupes|
+      # Keep the dupe with the shortest filename
+      trash dupes.sort_by { |p| File.basename(p).length }[1..-1]
+    end
+  end
+
+  #
+  # Screenshots
+  #
 
   rule 'Delete unused Screenshots' do
     destination = '~/Pictures/Screenshots/old/'
