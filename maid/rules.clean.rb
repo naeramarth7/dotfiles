33cac7c8cc8c991eb8b3b4adacfa31fe7b3ecfe1
Sven Flickinger <hc4e2k@gmail.com>
2016-09-12 19:11:05 +0800

    update main clean, add node and npm steps

--- maid/rules.clean.rb
+++ maid/rules.clean.rb
@@ -15,38 +15,51 @@ Maid.rules do
   # Downloads
   #
 
-  rule 'Trash disk images from ~/Downloads' do
-    trash(dir('~/Downloads/*.dmg'))
-  end
+  # rule 'Trash disk images from ~/Downloads' do
+  #   trash(dir('~/Downloads/*.dmg'))
+  # end
 
-  rule 'Trash applications in zip files from ~/Downloads' do
-    found = dir('~/Downloads/*.zip').select { |path|
-      zipfile_contents(path).any? { |c| c.match(/\.app$/) }
-    }
+  # rule 'Trash applications in zip files from ~/Downloads' do
+  #   found = dir('~/Downloads/*.zip').select { |path|
+  #     zipfile_contents(path).any? { |c| c.match(/\.app$/) }
+  #   }
 
-    trash(found)
-  end
+  #   trash(found)
+  # end
 
-  rule 'Trash duplicate downloads' do
-    dupes_in('~/Downloads/*').each do |dupes|
-      # Keep the dupe with the shortest filename
-      trash dupes.sort_by { |p| File.basename(p).length }[1..-1]
-    end
-  end
+  # rule 'Trash duplicate downloads' do
+  #   dupes_in('~/Downloads/*').each do |dupes|
+  #     # Keep the dupe with the shortest filename
+  #     trash dupes.sort_by { |p| File.basename(p).length }[1..-1]
+  #   end
+  # end
 
   #
   # Screenshots
   #
 
   rule 'Delete unused Screenshots' do
-    destination = '~/Pictures/Screenshots/old/'
+    destination = '~/Pictures/Others/Screenshots/old/'
     mkdir(destination)
 
-    dir('~/Pictures/Screenshots/Screen Shot *').each do |path|
+    dir('~/Pictures/Others/Screenshots/Screen Shot *').each do |path|
       if 1.week.since?(modified_at(path))
 
         move(path, destination)
       end
     end
   end
+
+  #
+  # Caches
+  #
+
+  rule 'Delete npm cache' do
+    log(`npm cache clean`)
+  end
+
+  rule 'Delete node-gyp cache' do
+    trash(dir('~/.node-gyp'))
+  end
+
 end
