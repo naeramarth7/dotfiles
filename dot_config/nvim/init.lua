3ca385705daad31b22263cf135ebf358bfe59872
Sven Herrle <sven@herrle.me>
2026-02-06 12:34:08 +0100

    chore(nvim): use ayu color theme

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -73,7 +73,7 @@ vim.o.inccommand = "split"
 vim.o.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 100
+vim.o.scrolloff = 10
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
@@ -729,20 +729,34 @@ require("lazy").setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    "folke/tokyonight.nvim",
+    "Shatur/neovim-ayu",
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require("tokyonight").setup({
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
+      require("ayu").setup({
+        mirage = false,
+        terminal = true,
+        overrides = {
+          Comment = { italic = false },
+          Normal = { bg = "None" },
+          NormalFloat = { bg = "none" },
+          ColorColumn = { bg = "None" },
+          SignColumn = { bg = "None" },
+          Folded = { bg = "None" },
+          FoldColumn = { bg = "None" },
+          CursorLine = { bg = "None" },
+          CursorColumn = { bg = "None" },
+          VertSplit = { bg = "None" },
         },
       })
 
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme("tokyonight-night")
+      -- require("lualine").setup({
+      --   options = {
+      --     theme = "ayu"
+      --   },
+      -- })
+
+      vim.cmd.colorscheme("ayu")
     end,
   },
 
