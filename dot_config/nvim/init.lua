66bd9e56c3d2d45700b4a22bb85449c6c0635813
Sven Herrle <sven@herrle.me>
2026-02-07 14:07:50 +0100

    chore(nvim): adjust custom plugins

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -840,7 +840,7 @@ require("lazy").setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
+  -- require "kickstart.plugins.indent_line",
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
@@ -850,7 +850,7 @@ require("lazy").setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
