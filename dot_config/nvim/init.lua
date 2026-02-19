a2a7d448ae9b60383ebe14a1f4a6995604bce0d3
Sven Herrle <sven@herrle.me>
2026-02-19 22:34:09 +0100

    chore(vim): include default xsh package

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -850,7 +850,7 @@ require("lazy").setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  { import = 'custom.plugins' },
+  { import = "custom.plugins" },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
@@ -877,6 +877,10 @@ require("lazy").setup({
     },
   },
 })
+
+--
+
+require("xsh7.remap")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
