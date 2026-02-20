973745604e3e24261f50b1bdc928afbd94b1f8a1
Sven Herrle <sven@herrle.me>
2026-02-20 11:43:07 +0100

    chore(darwin): fix ssh socket path

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -880,7 +880,7 @@ require("lazy").setup({
 
 --
 
-require("xsh7.remap")
+-- require("xsh7.remap")
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
